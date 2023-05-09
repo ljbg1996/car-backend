4557762a4ca60e6884fbf763e5a35dac5ea227ae
Martin Dorwarth <martin.dorwarth@valtech.com>
2023-05-09 15:45:46 +0200

    Uploaded files

---
+++ Phase 2/MoTraCoCa/src/main/java/com/MoTraCoCa/MoTraCoCa/MoTraCoCaApplication.java
@@ -0,0 +1,13 @@
+package com.MoTraCoCa.MoTraCoCa;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MoTraCoCaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MoTraCoCaApplication.class, args);
+	}
+
+}
---
+++ Phase 2/MoTraCoCa/src/test/java/com/MoTraCoCa/MoTraCoCa/MoTraCoCaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.MoTraCoCa.MoTraCoCa;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MoTraCoCaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
