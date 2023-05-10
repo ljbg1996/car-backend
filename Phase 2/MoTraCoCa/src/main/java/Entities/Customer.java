73e883deb28baec4ff0e9bf123cd08a9a3c4189f
Martin Dorwath <martin.dorwath@valtech.com>
2023-05-10 13:14:31 +0200

    Code with me Session changes by luebia

--- Phase 2/MoTraCoCa/src/main/java/Entities/Customer.java
+++ Phase 2/MoTraCoCa/src/main/java/Entities/Customer.java
@@ -1,4 +1,25 @@
 package Entities;
 
+import lombok.Getter;
+import lombok.RequiredArgsConstructor;
+
+import javax.persistence.*;
+import java.util.List;
+
+@Getter
+@RequiredArgsConstructor
+@Entity
 public class Customer {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    @Column(name = "id", nullable = false)
+    private final long id;
+    //paymentInfo
+    @OneToMany
+    private List<Vehicle> vehicleList;
+    @OneToMany
+    private List<Order> orderList;
+
+
 }
--- Phase 2/MoTraCoCa/src/main/java/Entities/Vehicle.java
+++ Phase 2/MoTraCoCa/src/main/java/Entities/Vehicle.java
@@ -3,13 +3,21 @@ package Entities;
 import lombok.Getter;
 import lombok.RequiredArgsConstructor;
 
+import javax.persistence.*;
 import java.util.List;
 
 @Getter
 @RequiredArgsConstructor
+@Entity
 public class Vehicle {
 
+    // id? (einzige Klasse ohne id)
+    @Id
+    // @GeneratedValue(strategy = GenerationType.AUTO) // Autogeneration nicht möglich bei vin
+    @Column(name = "vin", nullable = false)
     private final Vin vin;
+
+    @OneToMany
     private List<VehicleService> vehicleServiceList;
 
 
