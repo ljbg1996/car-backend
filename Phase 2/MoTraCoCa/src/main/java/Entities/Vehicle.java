9389bf4e77bd5b62e91e033f5bd9c4ad02b1c503
bianca.lueders <bianca.lueders@valtech.com>
2023-05-10 10:07:08 +0200

    Vehicle

--- Phase 2/MoTraCoCa/src/main/java/Entities/Vehicle.java
+++ Phase 2/MoTraCoCa/src/main/java/Entities/Vehicle.java
@@ -3,9 +3,14 @@ package Entities;
 import lombok.Getter;
 import lombok.RequiredArgsConstructor;
 
+import java.util.List;
+
 @Getter
 @RequiredArgsConstructor
 public class Vehicle {
+
+    private final Vin vin;
+    private List<VehicleService> vehicleServiceList;
 
 
 }
