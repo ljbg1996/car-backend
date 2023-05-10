90ab78bd4553dd3f1100152e918cf77f6e23dcbb
Martin Dorwath <martin.dorwath@valtech.com>
2023-05-10 13:14:01 +0200

    Code with me Session changes by aldinsab

--- Phase 2/MoTraCoCa/src/main/java/Entities/Order.java
+++ Phase 2/MoTraCoCa/src/main/java/Entities/Order.java
@@ -1,5 +1,27 @@
 package Entities;
 
+import lombok.Getter;
+import lombok.RequiredArgsConstructor;
+
+import javax.persistence.*;
+import java.util.Date;
+
+
+@Getter
+@RequiredArgsConstructor
+@Entity
 public class Order {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    @Column(name = "id", nullable = false)
+    private final long id;
+    private boolean isPayed;
+    @OneToOne
+    private final Vehicle vehicle;
+    private Price totalPrice;
+    private final Date startDate;
+    //ProductOrder als eigene Klasse
+
 
 }
