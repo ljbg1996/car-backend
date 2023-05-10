c319ba49a14507d034db813ca3d780c2eb336d88
bianca.lueders <bianca.lueders@valtech.com>
2023-05-10 10:30:22 +0200

    Class.puml

--- Phase 2/MoTraCoCa/src/main/java/Entities/VehicleService.java
+++ Phase 2/MoTraCoCa/src/main/java/Entities/VehicleService.java
@@ -1,4 +1,16 @@
 package Entities;
 
+import lombok.Getter;
+import lombok.RequiredArgsConstructor;
+
+import java.util.List;
+
+@Getter
+@RequiredArgsConstructor
 public class VehicleService {
+
+    private final long id;
+    private final Service implementedService;
+    private final List<UsageRight> usageRightList;
+
 }
