48636987d96917c2e60ea779643ca612f49d5f8f
bianca.lueders <bianca.lueders@valtech.com>
2023-05-16 16:49:20 +0200

    POM aktualisiert

--- Phase 2/MoTraCoCa/src/main/java/com/motracoca/repositorys/CustomerRepository.java
+++ Phase 2/MoTraCoCa/src/main/java/com/motracoca/repositorys/CustomerRepository.java
@@ -2,6 +2,8 @@ package com.motracoca.repositorys;
 
 import com.motracoca.entities.CustomerEntity;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface CustomerRepository extends JpaRepository<CustomerEntity, Long> {
 }
