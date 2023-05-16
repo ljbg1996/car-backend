e4fb77961cabdac107b013c82999b5700ecd4563
bianca.lueders <bianca.lueders@valtech.com>
2023-05-16 10:15:01 +0200

    Imports aufräumen

--- Phase 2/MoTraCoCa/src/test/java/com/MoTraCoCa/motracoca/repositorys/CustomerEntityRepositoryTest.java
+++ Phase 2/MoTraCoCa/src/test/java/com/MoTraCoCa/motracoca/repositorys/CustomerEntityRepositoryTest.java
@@ -2,20 +2,16 @@ package com.motracoca.motracoca.repositorys;
 
 import com.motracoca.motracoca.entities.CustomerEntity;
 import com.motracoca.motracoca.entities.VehicleEntity;
-
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
-
 import java.util.List;
-
 import static org.assertj.core.api.Assertions.assertThat;
 
 @SpringBootTest
 public class CustomerEntityRepositoryTest {
 
 
-    //private EntityManagerFactory emf = Persistence.createEntityManagerFactory("motracoca");
 
     @Autowired
     private CustomerRepository cr;
@@ -26,7 +22,8 @@ public class CustomerEntityRepositoryTest {
     void storeCustomerTest() {
 
 
-        VehicleEntity v1 = new VehicleEntity();
+
+        /*VehicleEntity v1 = new VehicleEntity();
 
         CustomerEntity c1 = new CustomerEntity();
         c1.getVehicleEntityList().add(v1);
@@ -37,7 +34,7 @@ public class CustomerEntityRepositoryTest {
         c2.getVehicleEntityList().add(v2);
 
         cr.save(c1);
-        cr.save(c2);
+        cr.save(c2);*/
 
 
         final List<CustomerEntity> customerEntityList = cr.findAll();
