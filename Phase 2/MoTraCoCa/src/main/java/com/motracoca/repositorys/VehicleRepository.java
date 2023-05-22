828243bbae9136b65d3f4ecb26abbbd085fd8077
bianca.lueders <bianca.lueders@valtech.com>
2023-05-22 15:22:19 +0200

    Changes by aldinsab

--- Phase 2/MoTraCoCa/src/main/java/com/motracoca/repositorys/VehicleRepository.java
+++ Phase 2/MoTraCoCa/src/main/java/com/motracoca/repositorys/VehicleRepository.java
@@ -1,4 +1,7 @@
 package com.motracoca.repositorys;
 
-public interface VehicleRepository {
+import com.motracoca.entities.VehicleEntity;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface VehicleRepository extends JpaRepository<VehicleEntity, String> {
 }
--- Phase 2/MoTraCoCa/src/test/java/com/motracoca/store/VehicleStoreTest.java
+++ Phase 2/MoTraCoCa/src/test/java/com/motracoca/store/VehicleStoreTest.java
@@ -1,4 +1,2 @@
 package com.motracoca.store;
 
-public class VehicleStoreTest {
-}
