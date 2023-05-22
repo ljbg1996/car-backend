25afa9c3d656a72b4e8c60ac8eb24acb0533f29a
bianca.lueders <bianca.lueders@valtech.com>
2023-05-22 15:22:30 +0200

    Changes by Martin

--- Phase 2/MoTraCoCa/src/test/java/com/motracoca/services/ActiveServiceServiceTest.java
+++ Phase 2/MoTraCoCa/src/test/java/com/motracoca/services/ActiveServiceServiceTest.java
@@ -1,5 +1,39 @@
 package com.motracoca.services;
 
+import com.motracoca.entities.CustomerEntity;
+import com.motracoca.entities.ServiceEntity;
+import com.motracoca.entities.VehicleEntity;
+import com.motracoca.repositorys.CustomerRepository;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+
+import java.util.List;
+
 public class ActiveServiceServiceTest {
-    
+
+    CustomerRepository customerRepository;
+
+    @Test
+    public void getActiveServicesTest(){
+        ActiveServiceService activeServiceService = new ActiveServiceService();
+
+
+        VehicleEntity v1 = new VehicleEntity();
+
+        CustomerEntity c1 = new CustomerEntity();
+
+        ServiceEntity s1 = new ServiceEntity();
+        ServiceEntity s2 = new ServiceEntity();
+
+        v1.setServiceEntityList(List.of(s1,s2));
+
+        c1.setVehicleEntityList(List.of(v1));
+
+
+
+        customerRepository.save(c1);
+
+
+    }
 }
