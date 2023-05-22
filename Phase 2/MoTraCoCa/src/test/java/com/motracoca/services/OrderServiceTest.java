54dc5b2c81a157feb9f34ae68a48df90e57909d0
bianca.lueders <bianca.lueders@valtech.com>
2023-05-22 15:24:26 +0200

    test bianca

--- Phase 2/MoTraCoCa/src/test/java/com/motracoca/services/OrderServiceTest.java
+++ Phase 2/MoTraCoCa/src/test/java/com/motracoca/services/OrderServiceTest.java
@@ -2,12 +2,16 @@ package com.motracoca.services;
 
 import com.motracoca.controller.OrderRESTController;
 import com.motracoca.entities.CustomerEntity;
+import com.motracoca.entities.OrderEntity;
 import com.motracoca.entities.VehicleEntity;
 import com.motracoca.repositorys.CustomerRepository;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+
+import java.util.List;
+
 import static org.assertj.core.api.Assertions.assertThat;
 
 
@@ -28,10 +32,18 @@ public class OrderServiceTest {
         CustomerEntity c1 = new CustomerEntity();
         c1.getVehicleEntityList().add(v1);
         c1.getVehicleEntityList().add(v2);
+        List<OrderEntity> orderList1 = c1.getOrderEntityList();
 
         cr.save(c1);
 
         orderService.buy();
+
+        CustomerEntity customerFromDb = cr.getById(c1.getId());
+        List<OrderEntity> orderList2 = customerFromDb.getOrderEntityList();
+        OrderEntity orderFromCustomerC1 = customerFromDb.getOrderEntityList().get(customerFromDb.getOrderEntityList().size()-1);
+
+        assertThat(orderList1.size()+1).isEqualTo(orderList2);
+        assertThat(orderFromCustomerC1).isNotNull();
     }
 
 
