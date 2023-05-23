8b52a234c20b6727412727de7b7b6693c6de1dc3
bianca.lueders <bianca.lueders@valtech.com>
2023-05-23 08:34:08 +0200

    Changes by aldinsab

--- Phase 2/MoTraCoCa/src/main/java/com/motracoca/services/OrderService.java
+++ Phase 2/MoTraCoCa/src/main/java/com/motracoca/services/OrderService.java
@@ -2,4 +2,32 @@ package com.motracoca.services;
 
 public class OrderService {
 
+    public void buy(List<String> articleNumberList, String vin, long customerId, int duration) {
+
+        Customer c = cs.getCustomerById(customerId);
+        List<Product> products = new ArrayList<>();
+
+        for (String articleNumber : articleNumberList) {
+            Product p = ps.getProductByArticleNumber(articleNumber);
+            products.add(p);
+        }
+
+        List<ProductConfiguration> productConfigurationList = new ArrayList<>();
+
+        for (Product p : products) {
+
+            ProductConfiguration pc = new ProductConfiguration(p, duration);
+            productConfigurationList.add(pc);
+
+        }
+
+
+
+
+
+
+
+
+
+    }
 }
--- Phase 2/MoTraCoCa/src/test/java/com/motracoca/store/VehicleStoreTest.java
+++ Phase 2/MoTraCoCa/src/test/java/com/motracoca/store/VehicleStoreTest.java
@@ -1,2 +1,45 @@
 package com.motracoca.store;
 
+import com.motracoca.entities.VehicleEntity;
+import com.motracoca.repositorys.VehicleRepository;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.mockito.Mock;
+
+import static org.mockito.Mockito.*;
+
+
+class VehicleStoreTest {
+
+    @Mock
+    private VehicleRepository vehicleRepository;
+
+    private VehicleStore vehicleStore;
+
+    @BeforeEach
+    void setup() {
+        vehicleStore = new VehicleStore(vehicleRepository);
+    }
+
+    @Test
+    void testSaveVehicle(){
+
+        VehicleEntity vehicle = new VehicleEntity();
+        vehicle.setVin("AB12");
+        vehicle.setId(Long.parseLong("A1899HU387N2k89BF124DF34AS"));
+        when(vehicleRepository.save(vehicle)).thenReturn(vehicle);
+
+        vehicleStore.saveVehicle(vehicle);
+
+        verify(vehicleRepository, times(1)).save(vehicle);
+
+
+
+    }
+
+
+
+
+
+}
+
