3e4559a5dd34b75b9d99d8696db8c420f491b4df
bianca.lueders <bianca.lueders@valtech.com>
2023-05-23 08:34:19 +0200

    test bianca

--- Phase 2/MoTraCoCa/src/main/java/com/motracoca/services/OrderService.java
+++ Phase 2/MoTraCoCa/src/main/java/com/motracoca/services/OrderService.java
@@ -1,6 +1,23 @@
 package com.motracoca.services;
 
+import com.motracoca.model.Customer;
+import com.motracoca.model.Product;
+import com.motracoca.model.ProductConfiguration;
+import com.motracoca.store.CustomerStore;
+import com.motracoca.store.ProductStore;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@Service
+@RequiredArgsConstructor
 public class OrderService {
+
+    private final CustomerStore cs;
+    private final ProductStore ps;
+
 
     public void buy(List<String> articleNumberList, String vin, long customerId, int duration) {
 
