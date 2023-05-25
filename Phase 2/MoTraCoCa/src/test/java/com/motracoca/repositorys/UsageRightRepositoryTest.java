bf3429e90221311cdcc676909a53525214a51b79
Martin Dorwath <martin.dorwath@valtech.com>
2023-05-25 08:43:53 +0200

    added UsageRightRepositoryTest

---
+++ Phase 2/MoTraCoCa/src/test/java/com/motracoca/repositorys/UsageRightRepositoryTest.java
@@ -0,0 +1,17 @@
+package com.motracoca.repositorys;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+
+public class UsageRightRepositoryTest {
+    @Autowired
+    private UsageRightRepository usageRightRepository;
+
+
+    @DisplayName("should store a UsageRight")
+    @Test
+    public void storeUsageRightTest() {
+
+    }
+}
