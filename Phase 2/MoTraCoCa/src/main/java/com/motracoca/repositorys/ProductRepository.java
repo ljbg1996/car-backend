56d509f4d0ac3f8bdba8c226c139a0bc6311aa03
Martin Dorwath <martin.dorwath@valtech.com>
2023-05-26 17:30:33 +0200

    Edit ProductRepository

--- Phase 2/MoTraCoCa/src/main/java/com/motracoca/repositorys/ProductRepository.java
+++ Phase 2/MoTraCoCa/src/main/java/com/motracoca/repositorys/ProductRepository.java
@@ -5,9 +5,10 @@ import com.motracoca.entities.ServiceEntity;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
+import org.springframework.stereotype.Repository;
 
 import java.util.List;
-
+@Repository
 public interface ProductRepository extends JpaRepository<ProductEntity, Long> {
 
     public ProductEntity findProductEntityByArticleNumber(long articleNumber);
