2f5bd4d9e31059ffe913e96b51be56d65d0f2865
bianca.lueders <bianca.lueders@valtech.com>
2023-05-31 17:02:35 +0200

    Changes by wangxinjie

--- Phase 2/MoTraCoCa/src/main/java/com/motracoca/controller/ActiveServiceRESTController.java
+++ Phase 2/MoTraCoCa/src/main/java/com/motracoca/controller/ActiveServiceRESTController.java
@@ -1,5 +1,11 @@
 /*package com.motracoca.controller;
 
+import com.motracoca.model.Service;
+import com.motracoca.model.Vin;
+import com.motracoca.repositorys.CustomerRepository;
+import com.motracoca.services.ActiveServiceService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import org.springframework.web.bind.annotation.GetMapping;
@@ -13,5 +19,35 @@ import io.swagger.v3.oas.annotations.media.Schema;
 import io.swagger.v3.oas.annotations.Operation;
 
 @RestController
+@RequestMapping("/services")
 public class ActiveServiceRESTController {
+
+        private final ActiveServiceService activeServiceService;
+
+        @Autowired
+        public ActiveServiceRESTController(ActiveServiceService activeServiceService, CustomerRepository customerRepository) {
+            this.activeServiceService = activeServiceService;
+        }
+
+        @Operation(summary = "Get service list by vin and customerId")
+         @ApiResponses(value = {
+            @ApiResponse(responseCode = "200", description = "Found the service",
+                    content = { @Content(mediaType = "application/json",
+                            schema = @Schema(implementation = Service.class)) }),
+            @ApiResponse(responseCode = "404", description = "Service not found",
+                    content = @Content) })
+
+        @GetMapping("/{vin}/{customerId}")
+        public List<Service> getActiveServices(@PathVariable String vin, @PathVariable String customerId) {
+            Vin vin1 = new Vin(vin);
+            long customerId1 = Long.parseLong(customerId);
+
+            // Call ActiveServiceService to retrieve active services
+            List<Service> activeServices = activeServiceService.getActiveServices(vin1, customerId1);
+
+            return activeServices;
+        }
 }
+
+
+*/
