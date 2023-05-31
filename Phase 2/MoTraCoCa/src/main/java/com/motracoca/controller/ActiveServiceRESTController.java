31693d6a54e1fc6036ca226bc3555aed427f5b2d
bianca.lueders <bianca.lueders@valtech.com>
2023-05-31 17:02:28 +0200

    Changes by Martin

--- Phase 2/MoTraCoCa/src/main/java/com/motracoca/controller/ActiveServiceRESTController.java
+++ Phase 2/MoTraCoCa/src/main/java/com/motracoca/controller/ActiveServiceRESTController.java
@@ -2,6 +2,16 @@
 
 import org.springframework.web.bind.annotation.RestController;
 
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+
+import java.util.List;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
+import io.swagger.v3.oas.annotations.media.Content;
+import io.swagger.v3.oas.annotations.media.Schema;
+import io.swagger.v3.oas.annotations.Operation;
+
 @RestController
 public class ActiveServiceRESTController {
 }
