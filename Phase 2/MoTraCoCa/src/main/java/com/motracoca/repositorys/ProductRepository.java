527fac77adeb30f822680b983cbcd395405ae267
bianca.lueders <bianca.lueders@valtech.com>
2023-06-02 10:32:18 +0200

    Repositories aufgeräumt

--- Phase 2/MoTraCoCa/src/main/java/com/motracoca/repositorys/ProductRepository.java
+++ Phase 2/MoTraCoCa/src/main/java/com/motracoca/repositorys/ProductRepository.java
@@ -4,9 +4,7 @@ import com.motracoca.entities.ProductEntity;
 import com.motracoca.entities.ServiceEntity;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
-import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
-
 import java.util.List;
 
 @Repository
@@ -14,8 +12,6 @@ public interface ProductRepository extends JpaRepository<ProductEntity, Long> {
 
     public ProductEntity findProductEntityByArticleNumber(long articleNumber);
 
-    /*@Query("SELECT p FROM ProductEntity WHERE p.articleNumber =:articleNumber")
-    public ProductEntity getProductByArticleNumber(@Param("articleNumber") long articleNumber);*/
 
     @Query("SELECT DISTINCT product.includedServices FROM ProductEntity product JOIN FETCH ServiceEntity service")
     public List<ServiceEntity> findAllServices();
